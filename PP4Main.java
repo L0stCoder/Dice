9f4b7ba693173628056984ade8a8dd7675bc427f
Br3300 <45640200+Br3300@users.noreply.github.com>
2018-12-10 14:32:41 -0600

    Update file,
    adds fun comment

--- PP4Main.java
+++ PP4Main.java
@@ -1,6 +1,7 @@
 public class PP4Main{
     public static void main(String[]args){
     	PP4Sec dice=new PP4Sec();
+	    //This is a spicy test, lets bring some flavor (insert fire emoji here)
 		System.out.println("There was a total of "+dice.roll()+" \"BoxCars\"");
     }
 }
