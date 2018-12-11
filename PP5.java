c5e95855e3f42b71b92b9e951cd58b061a238678
L0stCoder <45640225+L0stCoder@users.noreply.github.com>
2018-12-11 14:35:22 -0600

    Update PP5.java

--- PP5.java
+++ PP5.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class PP5{
     public static void main(String[]args){
     	Scanner input=new Scanner(System.in);
+    	int x=1, y=1;
     	int Player1S=0, Player2S=0; //Starts score off at zero points
     	int Player1RS=0, Player2RS=0; //Adds up total points in the round
     	final int FINAL=100;
@@ -25,7 +26,9 @@ public class PP5{
    				System.out.println("Player 1's total score is: "+Player1S);
    				System.out.print("Would you like to roll? [y, n]: ");
     			Roll=input.nextLine();
-   				while(test.equalsIgnoreCase("y")){
+    			System.out.println("\n----------------------------------------\n");
+    			y=1;
+    			while(x==y){
     				if(Roll.equalsIgnoreCase("y")){
     					die1=(int)((Math.random()*6)+1);
     					die2=(int)((Math.random()*6)+1);
@@ -35,24 +38,59 @@ public class PP5{
     						if(die1==1&&die2==1){
     							Player1S=0;
     						}
-    						Turn="P2";
+    						y=0;
     					}
     					else{
     						Player1RS=Player1RS+(die1+die2);
     						System.out.println("Player 1's total score this round is: "+Player1RS);
     						System.out.print("Would you like to roll again and be a PIG? [y, n]: ");
-    						test=input.nextLine();
+    						Roll=input.nextLine();
     					}
     				}
     				else{
     					System.out.println("Player 1's turn has been skipped!");
+    					y=0;
     				}
-    				System.out.println("\n");
+    				System.out.println("\n----------------------------------------\n");
    				}
    				Player1S=Player1S+Player1RS;
    				Turn="P2";
     		}
     		else if(Turn.equalsIgnoreCase("P2")){
+    			Player1RS=0;
+   				System.out.println("It is currently Player 2's turn");
+   				System.out.println("Player 2's total score is: "+Player2S);
+   				System.out.print("Would you like to roll? [y, n]: ");
+    			Roll=input.nextLine();
+    			System.out.println("\n----------------------------------------\n");
+    			y=1;
+    			while(x==y){
+    				if(Roll.equalsIgnoreCase("y")){
+    					die1=(int)((Math.random()*6)+1);
+    					die2=(int)((Math.random()*6)+1);
+    					System.out.println("Player 2 rolled a "+die1+" on dice 1 and a "+die2+" on dice 2");
+    					if(die1==1||die2==1){
+    						Player2RS=0;
+    						if(die1==1&&die2==1){
+    							Player2S=0;
+    						}
+    						y=0;
+    					}
+    					else{
+    						Player2RS=Player2RS+(die1+die2);
+    						System.out.println("Player 2's total score this round is: "+Player2RS);
+    						System.out.print("Would you like to roll again and be a PIG? [y, n]: ");
+    						Roll=input.nextLine();
+    					}
+    				}
+    				else{
+    					System.out.println("Player 2's turn has been skipped!");
+    					y=0;
+    				}
+    				System.out.println("\n----------------------------------------\n");
+   				}
+   				Player2S=Player2S+Player2RS;
+   				Turn="P1";
     		}
     	}
     }
